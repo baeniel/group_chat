e34a9644da95fd1bea5b7a40e0908a0aa1882627
baeniel <jinveloper126@gmail.com>
2019-10-29 20:59:53 +0900

    chatroom_user controller 코드 짜기

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.4.2'
 
 gem 'devise'
 gem 'bootstrap-sass'
+gem 'devise-bootstrapped'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
---
+++ app/controllers/chatroom_users_controller.rb
@@ -0,0 +1,18 @@
+class ChatroomUsersController < ApplicationController
+  before_action :authenticate_user!, :set_chatroom
+
+  def create
+    @chatroom_user = @chatroom.chatroom_users.where(user: current_user).first_or_create
+    redirect_to chatrooms_path
+  end
+
+  def destroy
+    @chatroom_user = @chatroom.chatroom_users.where(user: current_user).destroy_all
+    redirect_to chatrooms_path
+  end
+
+  private
+  def set_chatroom
+    @chatroom = Chatroom.find params[:chatroom_id]
+  end
+end
