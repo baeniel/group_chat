1e26340f955bb372b7afd069dcf3e096cb9188cf
baeniel <jinveloper126@gmail.com>
2019-10-29 20:05:58 +0900

    modeling

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :messages
+  has_many :chatroom_users
+  has_many :chatrooms, through: :chatroom_users
+
+end
